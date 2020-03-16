2e45c6000be201896aeffea2d8f4be5c5b35e868
Augusto Alonso <augustoalonso98@gmail.comn>
2020-03-16 10:31:57 -0600

    remove trash

--- src/actions/selectedTrafficLight.js
+++
@@ -1,7 +0,0 @@
-import * as types from '../types/selectedTrafficLight';
-
-
-export const selectTrafficLight = index => ({
-  type: types.TRAFFIC_LIGHT_SELECTED,
-  payload: index,
-});
--- src/types/pmt.js
+++
@@ -1,2 +0,0 @@
-export const PMT_AGENT_ADDED = 'PMT_AGENT_ADDED';
-export const PMT_AGENT_CHANGED = 'PMT_AGENT_CHANGED';
